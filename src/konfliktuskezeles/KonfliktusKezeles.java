b5d0db3605bffdf034ba33b55c0f9ea32ee7522e
Norbus2000 <78794087+Norbus2000@users.noreply.github.com>
2022-03-07 14:46:52 +0100

    3. nev deklaralasa

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,9 +3,11 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
-
+        
+        int i = 7;
         String nev1 = "Ódor Norbert";
         String nev2 = "X.Y.";
+        int nev3;
         
     }
     
